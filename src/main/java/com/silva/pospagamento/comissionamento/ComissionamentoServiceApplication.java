3e830a0635d5bdfb8846ac7a1157e6d830dce3b0
Dorival Querino da Silva <dorival.querino@gmail.com>
2023-02-27 23:04:41 -0300

    first commit

---
+++ src/main/java/com/silva/pospagamento/comissionamento/ComissionamentoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.silva.pospagamento.comissionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComissionamentoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComissionamentoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silva/pospagamento/comissionamento/ComissionamentoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silva.pospagamento.comissionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComissionamentoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
